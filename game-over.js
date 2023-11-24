2ab43b64e01416f9156cc9f14a5eb19e631a40b8
Stephanie Karlsson <Steffihenke@hotmail.se>
2023-11-24 12:41:27 +0100

    testfunktion för gameover vyn

---
+++ game-over.js
@@ -0,0 +1,27 @@
+// SPELVY
+// Variabler
+const testBtn = document.querySelector('#test-btn');
+const gameView = document.querySelector('.window');
+const gameOver = document.querySelector('#game-over');
+const testBtnBack = document.querySelector('#test-btn-back');
+
+// Skapar en funktion där spelvyn tas bort och game over vyn tas fram när man klickar på en knapp
+function changeView() {
+
+    gameView.style.display = 'none'
+    gameOver.style.display = 'block'
+
+}
+// Anropar funktionen
+testBtn.addEventListener ('click', changeView);
+
+// Skapar en funktion där gameover vyn tas bort och spelvyn vyn tas fram när man klickar på en knapp
+function changeViewBack() {
+
+    gameOver.style.display = 'none'
+    gameView.style.display = 'block'
+
+}
+
+// Anropar funktionen
+testBtnBack.addEventListener ('click', changeViewBack);
