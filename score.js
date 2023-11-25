b8ca37d6d31df59dd472588a649aa35f759d6fbd
AzrSara <137381182+AzrSara@users.noreply.github.com>
2023-11-25 02:04:56 +0100

    Gjorde en poängvy

--- score.js
+++ score.js
@@ -1,27 +1,35 @@
-// SPELVY
-// Variabler
-export const testBtn = document.querySelector('#test-btn');
-export const gameView = document.querySelector('.window');
-export const gameOver = document.querySelector('#game-over');
-export const newGame = document.querySelector('.game-view');
+document.addEventListener('DOMContentLoaded', function () {
+  // Existerande kod
+  const scoreView = document.querySelector('#score-view');
+  const button1 = document.querySelector('.window');
+  const score = document.querySelector('#score');
+  const newGame = document.querySelector('.game-view');
+  const newGameButton = document.querySelector('.button1');
 
-// Skapar en funktion där spelvyn tas bort och game over vyn tas fram när man klickar på en knapp
-export function changeView() {
+  // Skapar en funktion där spelvyn tas bort och score vyn tas fram när man klickar på en knapp
+  function changeView() {
+      button1.style.display = 'none';
+      score.style.display = 'block';
+  }
 
-    gameView.style.display = 'none'
-    gameOver.style.display = 'block'
+  // Anropar funktionen när #score-view klickas
+  scoreView.addEventListener('click', changeView);
 
-}
-// Anropar funktionen
-testBtn.addEventListener ('click', changeView);
+  // Skapar en funktion där score vyn tas bort och spelvyn vyn tas fram när man klickar på en knapp
+  function changeViewBack() {
+      score.style.display = 'none';
+      button1.style.display = 'block';
+  }
 
-// Skapar en funktion där gameover vyn tas bort och spelvyn vyn tas fram när man klickar på en knapp
-export function changeViewBack() {
+  // Anropar funktionen när .game-view klickas
+  newGame.addEventListener('click', changeViewBack);
 
-    gameOver.style.display = 'none'
-    gameView.style.display = 'block'
+  // Skapar en funktion där poängvyn tas bort och spelvyn tas fram när man klickar på "Starta nytt spel" i poängvyn
+  function startNewGame() {
+      score.style.display = 'none';
+      button1.style.display = 'block';
+  }
 
-}
-
-// Anropar funktionen
-newGame.addEventListener ('click', changeViewBack);
+  // Anropar funktionen när .button1 klickas
+  newGameButton.addEventListener('click', startNewGame);
+});
